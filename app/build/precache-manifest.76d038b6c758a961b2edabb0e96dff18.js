b7300c28984657b8cc5901fa88418378834886c4
haaryrichardt <haaryrichardt@gmail.com>
2020-08-05 15:13:27 +0530

    h

---
+++ app/build/precache-manifest.76d038b6c758a961b2edabb0e96dff18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12efe63f932f250bfe1d8d91ed4e8e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ccc4296b2c4962914b3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d76a43f6e3041459151b",
+    "url": "/static/js/2.e84036d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e84036d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccc4296b2c4962914b3",
+    "url": "/static/js/main.7d85b493.chunk.js"
+  },
+  {
+    "revision": "5b9ebf936984f269f076",
+    "url": "/static/js/runtime-main.78af6472.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
